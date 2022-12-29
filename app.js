b662cc0d3fab37cfd75f3be6a4979430536c65bd
Catherine Okang-Boye <katieboye44@gmail.com>
2022-12-29 03:37:17 +0000

    Added content

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// Defining variables for generate/ reset passwords and outputs
+var passwordEl = document.querySelector("#password-el")
+var outputEl1 = document.querySelector("#passwordOutputOne-el")
+var outputEl2 = document.querySelector("#passwordOutputTwo-el")
+var resetEl = document.querySelector("#reset-el")
+let passwordLength = 15
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+ "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e",
+ "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+ "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!",
+ "@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",
+ ":",";","<",">",".","?","/"];
+
+// Random Character function
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+//Generate password function
+passwordEl.addEventListener("click", function(){
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+       passwordEl.textContent += getRandomCharacter()         
+    }
+})
+
+//Password reset function
+resetEl.addEventListener("click", function(){
+    let reset = ""
+    outputEl1.textContent = reset
+    outputEl2.textContent = reset
+})
